0dd7844a4b552860ccd1ee1f71af8d189dfea973
Brett Cornell <brett.durcan@gmail.com>
2014-03-24 12:33:09 -0700

    initial commit

---
+++ simple_access.py
@@ -0,0 +1,27 @@
+from glob import iglob
+
+
+def expander(
+        dtype='ba',
+        tree='rrqDir/calibzip1',
+        base='/tera2/data3/cdmsbatsProd/R133/dataReleases/Prodv5-3_June2013/merged/',
+        data='all',
+        productions=['all'],
+        cut='',
+        cutrev=''):
+
+    pbase = base + data + '/' + cutrev + dtype + '/' + cut
+
+    if 'rrqDir' in tree:
+        fname = 'calib_Prodv5-3_{}_ba.root'
+    elif 'rqDir' in tree:
+        fname = 'merge_Prodv5-3_{}_ba.root'
+    elif 'cutDir' in tree:
+        fname = cut + '_{}_ba.root'
+    if productions == ['all']:
+        prod = '0[0-9][0-9][0-9][0-9][0-9]?'
+        ppath = iglob(pbase + fname.format(prod))
+    else:
+        ppath = (pbase + fname.format(i) for i in productions)
+
+    return ppath
